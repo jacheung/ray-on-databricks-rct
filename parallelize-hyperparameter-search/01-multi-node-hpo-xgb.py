3a311d40296a0ad666d20590bee6adc14df13c92
jacheung <jacheung>
2025-09-22 22:07:02 +0000

    Patch HPO search using Ray Data for XGBoost

--- parallelize-hyperparameter-search/01-multi-node-hpo-xgb.py
+++ parallelize-hyperparameter-search/01-multi-node-hpo-xgb.py
@@ -51,70 +51,45 @@
 
 # COMMAND ----------
 
-!pip install --quiet scikit-learn bayesian-optimization==1.5.1 ray[default]==2.42.0 ray[tune]==2.42.0 optuna mlflow==2.20.1
+!pip install --quiet xgboost bayesian-optimization==1.5.1 ray[all]=2.49.1 optuna mlflow
 dbutils.library.restartPython()
 
 
 # COMMAND ----------
 
-num_training_rows = 5_000_000
+# MAGIC %md 
+# MAGIC ## Parallelize hyperparameter tuning for XGboost
+# MAGIC To parallelize hyperparameter tuning we will perform two steps:
+# MAGIC - 1a. instantiate a Ray cluster - a Ray cluster is composed of multi-nodes for computing. Since this is Ray on Spark, we can assign `min/max worker_nodes` equal to (or less than) the number of worker nodes in the Spark cluster and `num_cpus_per_node` to the number of CPUs allocated per worker in the Spark cluster. 
+# MAGIC - 1b. Use Ray Tune to define and search the hyperparameter space. 
+
+# COMMAND ----------
+
+num_training_rows = 20_000_000
 num_training_columns = 100
 num_labels = 5
 
 catalog = "main"
 schema = "ray_gtm_examples"
-table = f"main.ray_gtm_examples.synthetic_data_{num_training_rows}_rows_{num_training_columns}_columns_{num_labels}_labels"
+table = f"synthetic_data_{num_training_rows}_rows_{num_training_columns}_columns_{num_labels}_labels"
 label="target"
+# Set mlflow experiment name
+mlflow_experiment_name = '/Users/jon.cheung@databricks.com/ray-xgb-nike-fresh'
+
+# Set the number of HPO trials to run
+num_samples = 48
+
+# If running in a multi-node cluster, this is where you
+# should configure the run's persistent storage that is accessible
+# across all worker nodes.
+ray_xgboost_path = '/dbfs/Users/jon.cheung@databricks.com/ray_xgboost/' 
+# This is for stashing the cluster logs
+ray_logs_path = "/dbfs/Users/jon.cheung@databricks.com/ray_collected_logs/"
 
 # COMMAND ----------
 
-data = spark.read.table(table).toPandas()
-labels = data.pop(label)
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC ## Baseline: one XGB model
-# MAGIC This short code-snippet below builds one XGBoost model using a specific set of hyperparameters. It'll provide a starting point for us before we parallelize. Ensure you understand what's going on here before we move onto the next steps. 
-
-# COMMAND ----------
-
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
-import numpy as np
-from sklearn.metrics import mean_squared_error
-
-# Perform train test split
-train_x, test_x, train_y, test_y = train_test_split(data, labels, test_size=0.25)
-
-# XGB sample hyperparameter configs
-config = {'objective':'multi:softmax',
-          'eval_metric': 'mlogloss', 
-          'learning_rate':0.05,
-          'n_estimators':1000,
-          'early_stopping_round': 20, 
-          'n_jobs': 16, 
-          'random_state':42}
-
-# Train the classifier
-bst = XGBClassifier(**config)
-bst.fit(train_x, train_y, eval_set=[(test_x, test_y)])
-
-# Retrieve the evaluation metric values from the training process
-results = bst.evals_result()
-final_eval_metric = results['validation_0'][config['eval_metric']][-1]
-final_eval_metric
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC ## 2. Parallelize hyperparameter tuning for XGboost
-# MAGIC To parallelize hyperparameter tuning we will perform two steps:
-# MAGIC - 2a. instantiate a Ray cluster - a Ray cluster is composed of multi-nodes for computing. Since this is Ray on Spark, we can assign `min/max worker_nodes` equal to (or less than) the number of worker nodes in the Spark cluster and `num_cpus_per_node` to the number of CPUs allocated per worker in the Spark cluster. 
-# MAGIC - 2b. Use Ray Tune to define and search the hyperparameter space. 
-# MAGIC
-# MAGIC ### 2a. Instantiate a Ray cluster
+# MAGIC %md
+# MAGIC ### 1a. Instantiate a Ray cluster
 # MAGIC
 # MAGIC The recommended configuration for a Ray cluster is as follows:
 # MAGIC - set the `num_cpus_per_node` to the CPU count per worker node (with this configuration, each Apache Spark worker node launches one Ray worker node that will fully utilize the resources of each Apache Spark worker node.)
@@ -125,6 +100,7 @@ final_eval_metric
 
 import ray
 from ray.util.spark import setup_ray_cluster, shutdown_ray_cluster
+import os
 
 restart = True
 if restart is True:
@@ -137,21 +113,67 @@ if restart is True:
   except:
     pass
 
+# Set the parameters here so mlflow works properly at all the Ray nodes
+os.environ['DATABRICKS_HOST'] = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get()
+os.environ['DATABRICKS_TOKEN'] = dbutils.secrets.get(scope = "development", key = "jon_cheung_PAT")
+
 # The below configuration mirrors my Spark worker cluster set up. Change this to match your cluster configuration. 
 setup_ray_cluster(
-  min_worker_nodes=2,
+  min_worker_nodes=8,
   max_worker_nodes=8,
-  num_cpus_per_node=16,
+  num_cpus_worker_node=48,
   num_gpus_worker_node=0,
   collect_log_to_path="/dbfs/Users/jon.cheung@databricks.com/ray_collected_logs"
 )
 
 # COMMAND ----------
 
+import ray
+import os
+
+try: 
+  ## Option 1 (PREFERRED): Build a Ray Dataset using a Databricks SQL Warehouse
+  # Insert your SQL warehouse ID here. I've queried my 100M row dataset using a Small t-shirt sized cluster.
+
+  # Ensure you've set the DATABRICKS_TOKEN so you can query using the warehouse compute
+  ds = ray.data.read_databricks_tables(
+    warehouse_id='2a72600bb68f00ee',
+    catalog=catalog,
+    schema=schema,
+    query=f'SELECT * FROM {table}',
+  )
+
+  print('read directly from UC')
+except: 
+  ## Option 2: Fallback to building a Ray Dataset using a Parquet files
+  # If you have too many Ray nodes, you may not be able to create a Ray dataset using the warehouse method above because of rate limits. One back up solution is to create parquet files from the delta table and build a ray dataset from that. This is not the recommended route because, in essence, you are duplicating data.
+  parquet_path = f'/Volumes/{catalog}/{schema}/synthetic_data/{table}'
+  ds = ray.data.read_parquet(parquet_path)
+  print('read directly from parquet')
+
+train_dataset, val_dataset = ds.train_test_split(test_size=0.25)
+
+# COMMAND ----------
+
 # MAGIC %md
-# MAGIC ### 2b. Use Ray Tune to parallelize hyperparameter search
-# MAGIC
-# MAGIC ![](images/xgboost_ray_tune.jpg)
+# MAGIC ### 1b. Use Ray Tune to parallelize hyperparameter search
+
+# COMMAND ----------
+
+num_cpu_per_trial=48
+
+# Define the hyperparameter search space.
+# XGB sample hyperparameter configs
+param_space = {
+    "objective": "multi:softmax", #TODO change to reg:squarederror for regression
+    'eval_metric': 'mlogloss', #TODO change to rmse/mae/rmsle for regression
+    "num_class": num_labels, #TODO remove for regression
+    "learning_rate": tune.uniform(0.01, 0.3),
+    "num_estimators": tune.randint(100, 1000),
+    "early_stopping_rounds": tune.randint(3, 20),
+    "n_jobs": num_cpu_per_trial, 
+    "random_state": 42
+}
 
 # COMMAND ----------
 
@@ -159,27 +181,27 @@ import os
 import numpy as np
 import mlflow
 from mlflow.utils.databricks_utils import get_databricks_env_vars
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_classification
-from xgboost import XGBClassifier
+import xgboost
 from ray import train, tune
 from ray.air.integrations.mlflow import MLflowLoggerCallback
 from ray.tune.search.optuna import OptunaSearch
+from ray.train.xgboost import XGBoostTrainer, RayTrainReportCallback
 
-# Set the number of trials to run
-num_samples = 48
+
+resources = ray.cluster_resources()
+total_cluster_cpus = resources.get("CPU") 
+max_concurrent_trials = max(1, int(total_cluster_cpus // num_cpu_per_trial))
+
 
 # Set mlflow experiment name
 experiment_name = '/Users/jon.cheung@databricks.com/ray-xgb-nike-fresh'
 mlflow.set_experiment(experiment_name)
-mlflow_db_creds = get_databricks_env_vars("databricks")
 
 # Define a training function to parallelize
-def train_classifier(config: dict,
-                    experiment_name: str,
-                    parent_run_id: str,
-                    mlflow_credentials: dict,
-                    ):
+def train_global_xgb(config: dict,
+                    train_data: ray.data.Dataset,
+                    val_data: ray.data.Dataset,
+                            ):
     """
     This objective function trains an XGBoost model given a set of sampled hyperparameters. There is no returned value but a metric that is sent back to the driver node to update the progress of the HPO run.
 
@@ -189,68 +211,60 @@ def train_classifier(config: dict,
     parent_run_id: str, the ID of the parent run. This is inherited from the driver node that initiates the mlflow parent run.
     mlflow_credentials: dict, the credentials for logging to mlflow. This is inherited from the driver node. 
     """
-    # Set mlflow credentials and active MLflow experiment within each Ray task
-    os.environ.update(mlflow_credentials)
-    mlflow.set_experiment(experiment_name)
+    # Convert Ray data to pandas DataFrame
+    # - materialize dumps the dataset from disk/cloud to cluster object store.
+    # - to_pandas pulls from object store to local node
+    train_df = train_data.materialize().to_pandas()
+    val_df = val_data.materialize().to_pandas()
+    
+    train_X = train_df.drop(label, axis=1)
+    train_y = train_df[label]
+    val_X = val_df.drop(label, axis=1)
+    val_y = val_df[label]
 
-    # Write code to import your dataset here
-    data, labels = make_classification(n_samples=1_000_000, 
-                                     n_features=100, 
-                                     n_informative=10, 
-                                     n_classes=3)
-    # Split into train and test set
-    train_x, test_x, train_y, test_y = train_test_split(data, labels, test_size=0.25)
-    with mlflow.start_run(run_name='xgb_model_hpo_250307', 
-                          nested=True,
-                          parent_run_id=parent_run_id):
+    dtrain = xgboost.DMatrix(train_X, label=train_y)
+    deval = xgboost.DMatrix(val_X, label=val_y)
 
-        # Train the classifier
-        bst = XGBClassifier(**config)
-        bst.fit(train_x, train_y, eval_set=[(test_x, test_y)], verbose=False)
+    evals_results = {}
+    bst = xgboost.train(
+        config,
+        dtrain=dtrain,
+        evals=[(deval, "validation")],
+        num_boost_round=config['num_estimators'],
+        evals_result=evals_results,
+        early_stopping_rounds=config['early_stopping_rounds']
+    )
+    eval_metric = evals_results['validation'][config['eval_metric']]
 
-        # Retrieve the evaluation metric values from the training process
-        eval_results = bst.evals_result()
-        final_eval_metric = eval_results['validation_0'][config['eval_metric']][-1]
-        
-        # write mlflow metrics
-        mlflow.log_params(config)
-        mlflow.log_metrics({f'validation_{config["eval_metric"]}': final_eval_metric})
+    tune.report({config['eval_metric']: eval_metric, "done": True})
 
-    # Return evaluation results back to driver node
-    train.report({config['eval_metric']: final_eval_metric, "done": True})
 
-# By default, Ray Tune uses 1 CPU/trial. LightGBM leverages hyper-threading so we will utilize all CPUs in a node per instance. Since I've set up my nodes to have 64 CPUs each, I'll set the "cpu" parameter to 64. Feel free to tune this down if you're seeing that you're not utilizing all the CPUs in the cluster. 
-trainable_with_resources = tune.with_resources(train_classifier, 
-                                               {"cpu": 16})
-
-# Define the hyperparameter search space.
-# XGB sample hyperparameter configs
-param_space = {
-    "objective": "multi:softmax",
-    'eval_metric': 'mlogloss', 
-    "learning_rate": tune.uniform(0.01, 0.3),
-    "n_estimators": tune.randint(100, 1000),
-    "early_stopping_rounds": tune.randint(3, 20),
-    "n_jobs": 16, 
-    "random_state": 42
-}
+# By default, Ray Tune uses 1 CPU/trial. XGB leverages hyper-threading so we will utilize all CPUs in a node per instance. Since I've set up my nodes to have 64 CPUs each, I'll set the "cpu" parameter to 64. Feel free to tune this down if you're seeing that you're not utilizing all the CPUs in the cluster. 
+trainable_with_resources = tune.with_resources(train_global_xgb, 
+                                               {"cpu": num_cpu_per_trial})
 
 # Set up search algorithm. Here we use Optuna and use the default the Bayesian sampler (i.e. TPES)
-optuna = OptunaSearch(metric="mlogloss", 
+optuna = OptunaSearch(metric=param_space['eval_metric'], 
                       mode="min")
 
-with mlflow.start_run(run_name ='5M_dataset_parallelized_16_cores') as parent_run:
+with mlflow.start_run(run_name ='20M_dataset_48_cores') as parent_run:
     tuner = tune.Tuner(
         ray.tune.with_parameters(
             trainable_with_resources,
-            experiment_name=experiment_name,
-            parent_run_id = parent_run.info.run_id,
-            mlflow_credentials=mlflow_db_creds),
+            train_data=train_dataset,
+            val_data=val_dataset),
+        run_config=tune.RunConfig(name='mlflow',
+                          callbacks=[MLflowLoggerCallback(
+                              experiment_name=mlflow_experiment_name,
+                              save_artifact=True,
+                              log_params_on_trial_end=True)]
+                          ),
         tune_config=tune.TuneConfig(num_samples=num_samples,
+                                    max_concurrent_trials=max_concurrent_trials,
                                     search_alg=optuna),
         param_space=param_space
         )
     results = tuner.fit()
 
-results.get_best_result(metric="mlogloss", 
+results.get_best_result(metric=param_space['eval_metric'], 
                         mode="min").config
